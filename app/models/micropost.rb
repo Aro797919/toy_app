b869e45669f6f61746090de6a3cde272469df649
Araik7979 <aro797919@gamil.com>
2017-07-09 17:28:23 +0300

    Add validates User

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to: user
+  belongs_to :user
   validates :content, length: {maximum: 140}, presence: true
   validates :user_id, numericality: {greater_than_or_equal_to: 0}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :microposts
+  validates :name,  uniqueness: true,  presence: true
+  validates :email, uniqueness: true,  presence: true
 end
