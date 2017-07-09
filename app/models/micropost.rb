b6208d0eaf523d2bcdd202f2f2f40e1855459202
Araik7979 <aro797919@gamil.com>
2017-07-09 17:15:43 +0300

    Add bunch user-microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
+  belongs_to: user
   validates :content, length: {maximum: 140}, presence: true
   validates :user_id, numericality: {greater_than_or_equal_to: 0}
 end
